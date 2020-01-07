643cc61320209340057c77fd0dddcf97ac6dceb8
ALatif8 <49005074+ALatif8@users.noreply.github.com>
2020-01-08 01:52:10 +0200

    Create form.php

---
+++ Assignment3/First_Question_Ecommerce_Calculator_without_login/form.php
@@ -0,0 +1,10 @@
+<form action = "carthandle.php" method = "post">
+
+<input type = "text" name = "product_price" placeholder = "Enter Product Price">
+<br/>
+<br/>
+<input type = "text" name = "product_count" placeholder = "Enter Product Pieces">
+<br/>
+<br/>
+<input type = "submit" value = "Go to Cart">
+</form>
