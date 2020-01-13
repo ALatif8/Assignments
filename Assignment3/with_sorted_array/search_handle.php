04f045d152aca7950a10caedfcfa38f3f38dc428
ALatif8 <49005074+ALatif8@users.noreply.github.com>
2020-01-14 01:35:29 +0200

    Update search_handle.php

--- Assignment3/with_sorted_array/search_handle.php
+++ Assignment3/with_sorted_array/search_handle.php
@@ -9,7 +9,7 @@ session_start();	//open session
 	else{
 		$search = $_POST['search']; //receive search_input from user
 		$list = array("tshirt"=>200 , "shirt"=>80 , "pantalon"=>220 , "shoes"=>120 , "short"=>90 , "jacket"=>60 , "glasses"=>80 , "sunglasses"=>100);
-		$search_flag = 0;
+		$search_flag = 0; //to flag that item found
 		foreach($list as $key => $value){ //iterate over the array
 			if(strstr($key, $search)==true){	//validate if any part of the search_input exist in the array
 				echo "item \t |\t price <br> $key \t |\t $value<br> "; //dispaly the item & its price
@@ -20,7 +20,7 @@ session_start();	//open session
 				break;	//exit the loop
 			}
 		}
-		if($search_flag == 0)
+		if($search_flag == 0) //item not found if flag = 0
 			echo "Item not found, <a href='search_items.php'>Go to Search!</a>";
 
 	}
